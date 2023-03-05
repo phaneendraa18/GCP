5263e218bce87858856f64a52593a5bf40d9a64d
phaneendraa18 <126634218+phaneendraa18@users.noreply.github.com>
2023-03-05 12:44:07 -0600

    buildfix- 4.6

--- assignment10/VmCreation.sh
+++ assignment10/VmCreation.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo $1 > token-file
+
 gcloud compute instances create wp-7 --project=$2 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=$3 --access-token-file=token-file
 gcloud compute instances delete wp-7 --zone=us-central1-a --access-token-file=token-file
