2cc8012b80b181063f07bc2b21165b541620aca5
root <root@phaneendra>
2023-03-05 12:11:57 -0600

    build fix 2.0

--- assignment10/VmCreation.sh
+++ assignment10/VmCreation.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo $1 > token-file
-gcloud compute instances create wp-7 --project=pj-project-374418 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=web-server --access-token-file=token-file
+echo $1
+echo $2
+echo $3
+#gcloud compute instances create wp-7 --project=pj-project-374418 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=web-server --access-token-file=token-file
+
