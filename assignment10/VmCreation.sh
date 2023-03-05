54161cfd17fdfb58fa85fbd315d48cab4afd7e27
root <root@phaneendra>
2023-03-05 11:56:17 -0600

    modifying CI.yml and VmCreation.sh file to run the build

--- assignment10/VmCreation.sh
+++ assignment10/VmCreation.sh
@@ -1,1 +1,4 @@
-gcloud compute networks create cloud-computing-practice --project=pj-project-374418 --region=asia-south1
+#!/usr/bin/env bash
+
+echo $1 > token-file
+gcloud compute instances create wp-7 --project=$2 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=$3 --access-token-file=token-file
