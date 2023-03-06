65bb6ad93283015ac9ac50c70a3dfd9f340b9bc7
phaneendraa18 <126634218+phaneendraa18@users.noreply.github.com>
2023-03-06 11:59:44 -0600

    Updated VmCreation.sh to check build

--- assignment10/VmCreation.sh
+++ assignment10/VmCreation.sh
@@ -3,6 +3,5 @@ echo $1 > token-file
 if gcloud compute instances list --project=$2 --access-token-file=token-file --filter="name=wp-7" | grep -q wp-7; then
   echo "Instance already exists."
 else
-  gcloud compute instances create wp-7 --project=$2 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=$3 --access-token-file=token-file
-  
+  gcloud compute instances create wp-7 --project=$2 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=$3 --access-token-file=token-file 
 fi
