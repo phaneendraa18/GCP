8200dba228d997529c39139ac9ee1f8c0a79a010
phaneendraa18 <126634218+phaneendraa18@users.noreply.github.com>
2023-03-06 11:53:04 -0600

    VmCreation Script modifications

--- assignment10/VmCreation.sh
+++ assignment10/VmCreation.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-echo $1 > token-file
-
-gcloud compute instances create wp-7 --project=$2 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=$3 --access-token-file=token-file
-gcloud compute instances delete wp-7 --zone=us-central1-a --project=$2 --access-token-file=token-file
+if gcloud compute instances list --project=$2 --access-token-file=token-file --filter="name=wp-7" | grep -q wp-7; then
+  echo "Instance already exists."
+else
+  echo $3 > token-file
+  gcloud compute instances create wp-7 --project=$2 --zone=us-central1-a --machine-type=e2-medium --network-interface=network-tier=PREMIUM,subnet=subnet-1 --tags=$3 --access-token-file=token-file
+  rm token-file
+fi
